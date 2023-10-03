20e013b819acb066bbc16b3a36b76c114049c4d5
Leolinson <135208930+Leolinson@users.noreply.github.com>
2023-10-03 23:23:39 +0200

    comments added to the calculation file

--- src/calculator.java
+++ src/calculator.java
@@ -2,18 +2,21 @@ public class calculator {
     public static void main(String[] args)
 
     {
-
+//Enter the group sizes insde the {} curly braces to input the data
         int[] groupsize={8,2,6};
+
+// variables for tablesneeded and avilables tables are defined
         int tablesneeded=0;
+//this array is to keep track of the number of available seats at each table
         int[] availabletables=new int[groupsize.length];
 
-
-
+//the first for loop is constructed to repeat and loop through each group
         for (int i=0; i<groupsize.length; i++)
         {
             int groupsizes = groupsize[i];
             boolean assigned = false;
-
+            
+// the secound for loop tries to assign the groups to an existing table
             for (int j=0; j<tablesneeded; j++)
             {
                 if(availabletables[j]+groupsizes<=8)
@@ -24,7 +27,7 @@ public class calculator {
                 }
             }
 
-
+// If there is no space in the existing tables a new Table is created and the group is assinged to this table
             if(!assigned)
             {
                 availabletables[tablesneeded]=groupsizes;
@@ -33,6 +36,7 @@ public class calculator {
 
 
         }
+// The total number of tables needed is calculated and is printed out
         System.out.println("tables needed: " + tablesneeded);
          
 
